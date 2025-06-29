09987d4b87eab3f15708a019f7403bf2ad0d821b
Mayank <mayankyt016@gmail.com>
2025-06-29 16:47:36 +0530

    Fixed layout issue and updated aspect ratio handling

--- app/src/main/java/com/devmayank/znap/MainActivity.kt
+++ app/src/main/java/com/devmayank/znap/MainActivity.kt
@@ -7,24 +7,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.devmayank.znap.ui.theme.ZnapTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window,false)
         enableEdgeToEdge()
         setContent {
             ZnapTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    permission()
+                    PermissionScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
 
                 }
             }
         }
     }
 }
+
 
